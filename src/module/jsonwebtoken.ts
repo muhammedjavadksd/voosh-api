dac3bb2b42efb5583c1f640f946217573e8d3b13
muhammedjavadksd <javadjazz899@gmail.com>
2024-12-08 20:47:27 +0530

    json web token module created

---
+++ src/module/jsonwebtoken.ts
@@ -0,0 +1,30 @@
+
+import jwt from 'jsonwebtoken';
+import { ITokenModule } from '../data/interface/abstractInterface';
+
+
+class JsonWebTokenModule implements ITokenModule {
+
+    async createToken(payload: Record<string, any>, expire: string): Promise<string | null> {
+        const secrets = process.env.TOKEN_SECRETS;
+        if (secrets) {
+            const token = jwt.sign(payload, secrets, { expiresIn: expire });
+            return token
+        } else {
+            throw new Error("Internals erver error")
+        }
+    }
+
+    async compareToken(token: string): Promise<Record<string, any> | null> {
+        const secrets = process.env.TOKEN_SECRETS;
+        if (secrets) {
+            const verify = jwt.verify(token, secrets);
+            return verify as Record<string, any>
+        } else {
+            throw new Error("Internals erver error")
+        }
+    }
+}
+
+
+export default JsonWebTokenModule
